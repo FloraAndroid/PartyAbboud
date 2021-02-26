eabb4c09c89ecb45245360498042432e5133398a
flora <flora.abboud@gmail.com>
2021-02-26 11:05:19 +0200

    authentication in viewmodel

---
+++ app/src/main/java/com/flora/partyabboud/ui/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.flora.partyabboud.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.flora.partyabboud.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
